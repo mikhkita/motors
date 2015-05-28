7dc5541890631aacdb67bd290211d2cfe011a66d
Михаил <beatbox787@gmail.com>
2015-05-28 08:28:52 +0600

    no message

--- protected/views/import/adminStep2.php
+++ protected/views/import/adminStep2.php
@@ -1,30 +1,28 @@
-<h1>Иморт - шаг 2</h1>
-
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'import-step2',
-	'action' => Yii::app()->createUrl('/Import/adminstep3'),
-	'enableAjaxValidation'=>false
-)); ?>
-
-
+<div class="b-import">
+    <h1>Импорт</h1>
+    <div class="progress">
+        <div class="progress-bar progress-bar-striped active" role="progressbar" style="width:3%">3%</div>
+    </div>
+    <ul class="b-log"></ul>
+</div>
+<div class="b-preview" style="display: none;" data-url="<?=Yii::app()->createUrl('/'.$this->adminMenu["cur"]->code.'/adminimport')?>">
 	<table class="b-table b-import-preview-table" border="1">
         <? foreach ($arr as $mark_name => $mark): ?>
             <tr> 
                 <td>
-                    <!-- <input type="hidden" name="mark[]" value="<?=$mark_name?>">  -->          
-                    <? foreach ($mark as $model_name => $model): ?>
-                        <? foreach ($model as $i => $engine): ?>
-                            <input type="hidden" name="<?=$mark_name?>['<?=$model_name?>'][]" value="<?=$engine['name']?>|<?=$engine['hp']?>">
+                    <input type="hidden" name="mark" value="<?=$mark["NAME"]?>">           
+                    <? foreach ($mark["MODELS"] as $model_name => $model): ?>
+                        <? foreach ($model["ENGINES"] as $i => $engine): ?>
+                            <input type="hidden" name="models[<?=$model["NAME"]?>][]" value="<?=mysql_real_escape_string(htmlspecialchars($engine['NAME']))?>|<?=$engine['HP']?>">
                         <? endforeach ?> 
                     <? endforeach ?>   
                 </td> 
             </tr>
         <? endforeach ?>
     </table>
-	<div class="row buttons">
+	<!-- <div class="row buttons">
 		<?php echo CHtml::submitButton('Перейти к предпросмотру'); ?>
 		<input type="hidden" name="excel_path" value="<?=$excel_path?>">
 		<input type="button" value="Отменить">
-	</div>
-
-<?php $this->endWidget(); ?>
+	</div> -->
+</div>
